6834aa40cceb872c03ecb2aea6c8ada355af317b
lekam0066 <87532266+lekam0066@users.noreply.github.com>
2023-05-16 16:49:17 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+  //hamburger menu click
+  $('#nav_toggle').click(function(){
+    $("#top-head").toggleClass('open');
+    $("nav").slideToggle(500);
+  });
+  //menu link click
+  $('nav a').click(function(){
+    if(window.innerWidth <= 680){
+      $("#top-head").toggleClass('open');
+      $("nav").slideToggle(500);
+    }
+  });
+});
